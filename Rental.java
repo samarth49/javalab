3a57cb16479df9e017e72b9e8b2e3d437e62ca2a
Samarth Otari <122641870+samarth49@users.noreply.github.com>
2024-05-20 01:11:44 +0530

    Add files via upload
    Interface file

---
+++ Rental.java
@@ -0,0 +1,10 @@
+//Define an interface called Rentable with methods like rent(), returnVehicle(), and calculateRentalCost().
+
+package Lab;
+
+public interface Rental {
+	 void rent();
+	  void returnVehicle();
+	 double calculateRentalCost(int rentalduration ) ;
+	
+}
